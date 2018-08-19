b31c1599583440193f15b47f7134f16561ead2aa
Matías Chau Ch <41970340+matiaschau@users.noreply.github.com>
2018-08-18 21:49:06 -0300

    Add files via upload

---
+++ Extractor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 18:58:31 2018
+
+@author: calocha
+"""
+
+import zipfile
+from os import listdir
+
+Dir1 = "/home/matias/Mis Documentos/UAndes/Métodos Computacionales en OOCC/Tareas/Proyecto 1/Registros Sismológicos/"
+Dir2 = "Zips/"
+
+sismos = listdir(Dir1+Dir2)
+
+for arch in sismos:
+    zipf = zipfile.ZipFile(Dir1+Dir2+arch)
+    contents = zipf.namelist()
+    for i,j in enumerate(contents):
+        final = contents[i][-7:]
+        if final == 'HNE.txt':
+            zipf.extract(contents[i], path=Dir1+"HNE")
+        elif final == 'HNN.txt':
+            zipf.extract(contents[i], path=Dir1+"HNN")
+        elif final == 'HNZ.txt':
+            zipf.extract(contents[i], path=Dir1+"HNZ")
+    
+
+
